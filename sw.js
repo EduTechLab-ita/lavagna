25766e0dbc9bf6fecce8420a5bca87dedc2887c3
EduTechLab.ita <edutechlab.ita@gmail.com>
2025-08-05 18:57:35 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,6 +5,8 @@ const urlsToCache = [
     '/lavagna/style.css',
     '/lavagna/app.js',
     '/lavagna/manifest.json',
+    '/lavagna/images/icon-192x192.png', // Aggiungiamo le icone principali alla cache
+    '/lavagna/images/icon-512x512.png',
     'https://cdnjs.cloudflare.com/ajax/libs/fabric.js/5.3.1/fabric.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.102/pdf.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.102/pdf.worker.min.js'
