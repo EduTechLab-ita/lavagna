863c16c5d53c9060fd1038f341a5edb882bc72de
EduTechLab.ita <edutechlab.ita@gmail.com>
2025-08-05 18:50:53 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'lavagna-pwa-cache-v1';
+const urlsToCache = [
+    '/lavagna/',
+    '/lavagna/index.html',
+    '/lavagna/style.css',
+    '/lavagna/app.js',
+    '/lavagna/manifest.json',
+    'https://cdnjs.cloudflare.com/ajax/libs/fabric.js/5.3.1/fabric.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.102/pdf.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.102/pdf.worker.min.js'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME )
+            .then(cache => {
+                console.log('Cache aperta');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
